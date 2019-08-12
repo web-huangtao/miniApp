1989a651595a4977e493429343943fb9e5696b7f
huangtao <huangtao3@youlanw.com>
2019-08-12 14:05:01 +0800

    路线

--- miniprogram/pages/map/index.js
+++ miniprogram/pages/map/index.js
@@ -7,12 +7,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    longitude: 0,
-    latitude: 0,
-    scale: 16,
-    markers: [],
-    polyline: [],
-    destination: '' // 目的地
+    longitude: 0, // 经度
+    latitude: 0, // 纬度
+    scale: 16, // 缩放比例
+    markers: [], // 添加标注
+    polyline: [], // 路线
+    currentPosition: '', // 当前位置
+    startPositionText: '我的位置', // 开始位置文案显示
+    startPosition: '', // 开始位置
+    endPosition: '', // 结束位置
+    startLongitude: 0, // 开始经度
+    startLatitude: 0, // 开始纬度
+    endLongitude: 0, // 结束经度
+    endLatitude: 0, // 结束纬度
+    isLine: false // 是否显示路线
   },
 
   /**
@@ -30,15 +38,21 @@ Page({
         const longitude = res.longitude
         this.setData({
           latitude: res.latitude,
-          longitude: res.longitude
+          longitude: res.longitude,
+          startLatitude: res.latitude,
+          startLongitude: res.longitude
         })
+        // 当前位置经纬度解析地址
         qqmapsdk.reverseGeocoder({
           location: {
             latitude: res.latitude,
             longitude: res.longitude
           },
           success: res => {
-            console.log(res)
+            this.setData({
+              currentPosition: res.result.address,
+              startPosition: res.result.address
+            })
           }
         })
       }
@@ -46,57 +60,155 @@ Page({
     // 使用 wx.createMapContext 获取 map 上下文
     this.mapCtx = wx.createMapContext('map')
   },
-  regionchange(e) {
-    // console.log(e)
+
+  /**
+   * 获取出发地
+   */
+  getStartPosition(e) {
+    this.setData({
+      startPositionText: e.detail.value,
+      startPosition: e.detail.value
+    })
+    const { startPositionText } = this.data
+    if (startPositionText !== '我的位置' && e.detail.value) {
+      this.analysis(e.detail.value).then(res => {
+        this.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          startLatitude: res.latitude,
+          startLongitude: res.longitude
+        })
+      })
+    }
   },
 
   /**
    * 获取目的地
    */
-  getDestination(e) {
+  getEndPosition(e) {
     this.setData({
-      destination: e.detail.value
+      endPosition: e.detail.value
+    })
+    if (e.detail.value) {
+      this.analysis(e.detail.value).then(res => {
+        this.setData({
+          endLatitude: res.latitude,
+          endLongitude: res.longitude
+        })
+      })
+    }
+  },
+
+  /**
+   * 地址解析经纬度
+   */
+  analysis(destination) {
+    return new Promise((resolve, reject) => {
+      qqmapsdk.geocoder({
+        address: destination,
+        success: res => {
+          var res = res.result
+          var latitude = res.location.lat
+          var longitude = res.location.lng
+          resolve({
+            latitude: res.location.lat,
+            longitude: res.location.lng,
+            title: res.title
+          })
+        },
+        fail: err => {
+          console.log(err)
+          reject(err)
+        }
+      })
     })
   },
 
   /**
-   * 解析地址，地图标注
+   * 确定按钮
    */
-  analysis() {
-    const { destination } = this.data
-    qqmapsdk.geocoder({
-      address: destination,
+  sure() {
+    const { startLatitude, startLongitude, endLatitude, endLongitude } = this.data
+    this.setData({
+      scale: 13,
+      markers: [{
+        id: 0,
+        latitude: startLatitude,
+        longitude: startLongitude,
+        iconPath: '/images/icon/start.png',//图标路径
+        width: 32,
+        height: 32
+      }, {
+        id: 1,
+        latitude: endLatitude,
+        longitude: endLongitude,
+        iconPath: '/images/icon/end.png',//图标路径
+        width: 32,
+        height: 32
+      }]
+    })
+    this.line()
+  },
+
+  /**
+   * 切换方式
+   */
+  onChange(e) {
+    let val = e.detail.index
+    switch(val) {
+      case 0:
+        this.line('driving')
+        break;
+      case 1:
+        this.line('transit')
+        break;
+      case 2:
+        this.line('bicycling')
+        break;
+      case 3:
+        this.line('walking')
+        break;
+    }
+  },
+
+  /**
+   * 路线规划
+   */
+  line(type) {
+    const { startLatitude, startLongitude, endLatitude, endLongitude } = this.data
+    qqmapsdk.direction({
+      mode: type ? type : '', // 可选值：'driving'（驾车）、'walking'（步行）、'bicycling'（骑行），不填默认：'driving',可不填
+      from: {
+        latitude: startLatitude,
+        longitude: startLongitude
+      },
+      to: {
+        latitude: endLatitude,
+        longitude: endLongitude
+      },
       success: res => {
-        var res = res.result
-        var latitude = res.location.lat
-        var longitude = res.location.lng
-        var pl = [{ 
-          latitude: this.data.latitude, 
-          longitude: this.data.longitude 
-        }, {
-          latitude: latitude, 
-          longitude: longitude
-        }]
+        console.log(res.result.routes)
+        if (type === 'transit') {
+          return
+        }
+        var coors = res.result.routes[0].polyline, pl = [];
+        //坐标解压（返回的点串坐标，通过前向差分进行压缩）
+        var kr = 1000000;
+        for (var i = 2; i < coors.length; i++) {
+          coors[i] = Number(coors[i - 2]) + Number(coors[i]) / kr;
+        }
+        //将解压后的坐标放入点串数组pl中
+        for (var i = 0; i < coors.length; i += 2) {
+          pl.push({ latitude: coors[i], longitude: coors[i + 1] })
+        }
         this.setData({
-          scale: 14,
-          markers: [{
-            id: 0,
-            title: res.title,
-            latitude: latitude,
-            longitude: longitude,
-            iconPath: '',//图标路径
-            width: 20,
-            height: 20
-          }],
+          isLine: true,
           polyline: [{
             points: pl,
             color: '#FF0000DD',
             width: 4
           }]
         })
-      },
-      fail: err => {
-        console.log(err)
       }
     })
   }
